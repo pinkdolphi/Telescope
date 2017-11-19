5ec2852677b55e9e5acb360afa95f1c61795a77a
pinkdolphi <quisquiliae@live.com>
2017-11-19 00:10:39 -0500

    initial stepper motor arduino code
    blink.ino for bipolar mode
    6wirestep.ino for unipolar mode

---
+++ 6wirestep/6wirestep/6wirestep.ino
@@ -0,0 +1,32 @@
+const int Pin6 = 6;    // connected to the base of the transistor
+const int Pin9 = 9;    // connected to the base of the transistor
+const int Pin10 = 10;    // connected to the base of the transistor
+const int Pin11 = 11;    // connected to the base of the transistor
+
+void setup() {
+   // set  the transistor pin as output:
+   pinMode(Pin6, OUTPUT);
+   pinMode(Pin9, OUTPUT);
+   pinMode(Pin10, OUTPUT);
+   pinMode(Pin11, OUTPUT);
+ }
+ 
+ void loop() {
+  int del=1000;
+   digitalWrite(Pin6, HIGH);
+   delay(del);
+   digitalWrite(Pin6, LOW);
+   delay(del);
+   digitalWrite(Pin9, HIGH);
+   delay(del);
+   digitalWrite(Pin9, LOW);
+   delay(del);
+   digitalWrite(Pin10, HIGH);
+   delay(del);
+   digitalWrite(Pin10, LOW);
+   delay(del);
+   digitalWrite(Pin11, HIGH);
+   delay(del);
+   digitalWrite(Pin11, LOW);
+   delay(del);
+ }
